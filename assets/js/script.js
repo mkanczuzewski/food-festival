ed78d73bacd93f67af985d4f2df1f26b3a9cdbca
mkanczuzewski <mkanczuzewski@gmail.com>
2022-02-08 10:26:52 -0500

    Optimize images and compare code to lesson 3 code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,26 @@
-require("bootstrap");
+require('bootstrap');
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
+
 $(document).ready(function() {
-
-
-
-    const pageEl = document.querySelector("#page");
-
-    function createCards(events) {
-      const cards = events.map((event) => (
-          createEl("div", {class: "card-body clickable", onClick: () => onEventClick(event)}, 
-          createEl("h5", {class: "card-title"}, event.title || ""),
-          createEl("p", {class: "card-text"}, event.description || createLoremIpsum()),
-          createEl("hr")
-          )
-      ))
-      return cards
-    }
-    
-    const containerEl1 = createEl("div", {class: "container mt-5"}, 
-    createEl("div", {class: "card mb-5"}, 
-        createEl("h5", {class: "card-header"}, "Day 1"),
-        ...createCards(events.slice(0,3))
-    )
-    )
-
-    const containerEl2 = createEl("div", {class: "container"}, 
-    createEl("div", {class: "card mb-5"}, 
-        createEl("h5", {class: "card-header"}, "Day 2"),
-        ...createCards(events.slice(3,6))
-    )
-    )
-
-    const containerEl3 = createEl("div", {class: "container"}, 
-    createEl("div", {class: "card mb-5"}, 
-        createEl("h5", {class: "card-header"}, "Day 3"),
-        ...createCards(events.slice(6,9))
-    )
-    )
-
-    pageEl.appendChild(containerEl1);
-    pageEl.appendChild(containerEl2);
-    pageEl.appendChild(containerEl3);
-  });
-
-  
-
-
-
-  
   // First image is hard coded in index.html
   const carouselSlides = [
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
